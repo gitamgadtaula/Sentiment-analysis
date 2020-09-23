fdd2fbb0812d71a62e97b383d6dcebf4f2641395
gitamgdtaula <gitamgadtaula@gmail.com>
2020-09-23 20:57:07 +0545

    added textblob package

--- sentiment.py
+++ sentiment.py
@@ -1,1 +1,6 @@
-print("hello worldfsdfsdfdsfsdd")
+from textblob import TextBlob
+
+str1 = 'i would hate you but you are my everything'
+str2 = 'i love you, you are so good, marvellous excellent for me, bravo, yes, wow ,amazingly'
+blob1 = TextBlob(str2)
+print(blob1.sentiment)
