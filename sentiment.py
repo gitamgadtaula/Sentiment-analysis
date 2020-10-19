bfc517d404811cc9ad9ce9e4df422ae6c6d527dc
Gitam Gadtaula <gitamgadtaula@gmail.com>
2020-10-19 20:24:24 +0545

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,8 @@
 from textblob import TextBlob
 
+
+//testing random string sentiment, later on a twitter string api will be generated and calculated for sentiments
+
 str1 = 'i would hate you but you are my everything'
 str2 = 'i love you, you are so good, marvellous excellent for me, bravo, yes, wow ,amazingly'
 blob1 = TextBlob(str2)
