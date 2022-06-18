e835f730e9a6a56b7134fd017d9d0ad2bf158a1c
Gitam Gadtaula <gitamgadtaula@Gitams-MacBook-Pro.local>
2022-06-18 14:59:26 +0545

    added a function to determine positive or negative sentiment

--- sentiment.py
+++ sentiment.py
@@ -1,19 +1,17 @@
-# import numpy as np  # for linear algebra
 import pandas as pd
-# import sklearn  # for dataprocessing; csv
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from spacy.lang.en.stop_words import STOP_WORDS
 from spacy.lang.en import English
 import string
 from sklearn.linear_model import LogisticRegression
-# import pdb
-# form sklearn.linear_model import
 import numpy as np
-from sklearn import tree
+# from sklearn import tree
 import os  # for environment variables
 import tweepy  # for twitter api
+import statistics  # for mean
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -32,7 +30,7 @@ dataset = pd.read_csv('combined_news.csv')
 finalNews = dataset['Top1'].values
 finalLabels = dataset['Label'].values
 for i in range(2, 23):
-    # merging all feature columns
+    # merging all feature columns of the dataset
     col = 'Top'+str(i)
     news = dataset[col].values
     finalNews = np.append(finalNews, news)
@@ -40,14 +38,14 @@ for i in range(2, 23):
 
 
 news_train, news_test, y_train, y_test = train_test_split(
-    finalNews, finalLabels, test_size=0.2, random_state=1000)
+    finalNews, finalLabels, test_size=0.2, random_state=1000)  # splitting test and train data
 
 punctuations = string.punctuation
 parser = English()
 stopwords = list(STOP_WORDS)
 
 
-# Using spaCy to Create a Custom Tokenizer (Optional)
+# Using spaCy to Create a Custom Tokenizer
 
 def spacy_tokenizer(utterance):
     tokens = parser(utterance)
@@ -63,19 +61,24 @@ X_train = vectorizer.transform(news_train)
 X_test = vectorizer.transform(news_test)
 
 classifier = LogisticRegression(max_iter=3000)
-# classifier = tree.DecisionTreeClassifier()
-# >>> clf = clf.fit(X, Y)
 classifier.fit(X_train, y_train)
 accuracy = classifier.score(X_test, y_test)
-print("Accuracy: ", accuracy)
+# print("Accuracy: ", accuracy)
 tweets = api.search_tweets(q='nepse', lang='en',
-                           count=3, tweet_mode="extended")
+                           count=20, tweet_mode="extended")
 search_results = [tweet.full_text
                   for tweet in tweets]  # mapping to fetch tweet.full_text only
-print(search_results)
-# search_results = ['Old version of python useless',
-#                   'Very good effort, but not five stars', 'plane crashed']
 X_new = vectorizer.transform(search_results)
-print(
-    classifier.predict(X_new)
-)
+prediction = classifier.predict(X_new)
+
+
+def sentiment_scores(score):
+    if score > 0.5:
+        print(" Positive sentiment")
+    else:
+        print(" Negative sentiment")
+
+
+meanSentiment = statistics.mean(prediction)
+print('______________________________________________')
+sentiment_scores(meanSentiment)
